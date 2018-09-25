89a348e3eda308250d3877c78091bb04e9c3d394
Eric Noulard <enoulard@antidot.net>
2018-09-25 19:50:12 +0200

    Add more informations about model, dump model structure in JSON and SVG

--- train.py
+++ train.py
@@ -5,9 +5,13 @@ import sys
 import logging
 import logging.handlers
 
+import json
+from pathlib import Path
+
 import numpy as np
 
 from model import build_model, save_weights
+from keras.utils.vis_utils import plot_model
 
 DATA_DIR = './data'
 LOG_DIR = './logs'
@@ -25,6 +29,7 @@ logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
 logger.addHandler(ch)
 logger.setLevel(logging.INFO)
 
+
 class TrainLogger(object):
     def __init__(self, file):
         self.file = os.path.join(LOG_DIR, file)
@@ -37,6 +42,7 @@ class TrainLogger(object):
         s = '{},{},{}\n'.format(self.epochs, loss, acc)
         with open(self.file, 'a') as f:
             f.write(s)
+
 
 def read_batches(T, vocab_size):
     length = T.shape[0]
@@ -51,20 +57,32 @@ def read_batches(T, vocab_size):
                 Y[batch_idx, i, T[batch_chars * batch_idx + start + i + 1]] = 1
         yield X, Y
 
+
 def train(text, epochs=100, save_freq=10):
-    char_to_idx = { ch: i for (i, ch) in enumerate(sorted(list(set(text)))) }
+    char_to_idx = {ch: i for (i, ch) in enumerate(sorted(list(set(text))))}
     with open(os.path.join(DATA_DIR, 'char_to_idx.json'), 'w') as f:
         json.dump(char_to_idx, f)
 
-    idx_to_char = { i: ch for (ch, i) in list(char_to_idx.items()) }
     vocab_size = len(char_to_idx)
 
+    logger.info("batch_size=%d, seq_length=%d, vocab_size=%d",
+                BATCH_SIZE, SEQ_LENGTH, vocab_size)
     model = build_model(BATCH_SIZE, SEQ_LENGTH, vocab_size)
     model.summary()
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    json.dump(json.loads(model.to_json()),
+              Path(DATA_DIR).joinpath('model.json').open('w'),
+              sort_keys=True,
+              indent=1
+              )
+
+    plot_model(model, to_file=os.path.join(DATA_DIR, 'model_plot.svg'), show_shapes=True, show_layer_names=True)
 
     T = np.asarray([char_to_idx[c] for c in text], dtype=np.int32)
     steps_per_epoch = (len(text) / BATCH_SIZE - 1) / SEQ_LENGTH
+    logger.info("steps_per_epoch=%d", steps_per_epoch)
     log = TrainLogger('training_log.csv')
 
     for epoch in range(epochs):
@@ -84,11 +102,16 @@ def train(text, epochs=100, save_freq=10):
             save_weights(epoch + 1, model)
             print('Saved checkpoint to', 'weights.{}.h5'.format(epoch + 1))
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train the model on some text.')
-    parser.add_argument('--input', default='input.txt', help='name of the text file to train from')
-    parser.add_argument('--epochs', type=int, default=100, help='number of epochs to train for')
-    parser.add_argument('--freq', type=int, default=10, help='checkpoint save frequency')
+    parser = argparse.ArgumentParser(
+        description='Train the model on some text.')
+    parser.add_argument('--input', default='input.txt',
+                        help='name of the text file to train from')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train for')
+    parser.add_argument('--freq', type=int, default=10,
+                        help='checkpoint save frequency')
     args = parser.parse_args()
 
     if not os.path.exists(LOG_DIR):
