f34309c1f144dca70540b2331c8d8248cd49fb7d
Eric Noulard <enoulard@antidot.net>
2018-09-27 22:33:55 +0200

    Add --resume option in order to resume training from last check point

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 import numpy as np
 
-from model import build_model, save_weights
+from model import build_model, save_weights, load_weights
 from keras.utils.vis_utils import plot_model
 
 DATA_DIR = './data'
@@ -32,9 +32,9 @@ logger.setLevel(logging.INFO)
 
 
 class TrainLogger(object):
-    def __init__(self, file):
+    def __init__(self, file, resume_at=0):
         self.file = os.path.join(LOG_DIR, file)
-        self.epochs = 0
+        self.epochs = resume_at
         with open(self.file, 'w') as f:
             f.write('epoch,loss,acc\n')
 
@@ -59,10 +59,30 @@ def read_batches(T, vocab_size):
         yield X, Y
 
 
-def train(text, epochs=100, save_freq=10):
-    char_to_idx = {ch: i for (i, ch) in enumerate(sorted(list(set(text))))}
-    with open(os.path.join(MODEL_DIR, 'char_to_idx.json'), 'w') as f:
-        json.dump(char_to_idx, f)
+def train(text, epochs=100, save_freq=10, resume=False):
+
+    if resume:
+        logger.info("Trying to resume last training...")
+        # try to resume previous training
+        model_dir = Path(MODEL_DIR)
+        # load the dictionnary (character to idx file)
+        c2ifile = model_dir.joinpath('char_to_idx.json')
+        with c2ifile.open('r') as f:
+            char_to_idx = json.load(f)
+        # list checkpoints files
+        lfw = model_dir.glob('weights.*.h5')
+        # get the list of saved epochs
+        lcp = map(lambda p: int(p.name.split('.h5')[0]
+                                      .split('weights.')[1]),
+                  lfw)
+        # the biggest one is the last one
+        last_epoch_checkpointed = max(lcp)
+        logger.info("Resuming from epoch %d",last_epoch_checkpointed)
+    else:
+        last_epoch_checkpointed = 0
+        char_to_idx = {ch: i for (i, ch) in enumerate(sorted(list(set(text))))}
+        with open(os.path.join(MODEL_DIR, 'char_to_idx.json'), 'w') as f:
+            json.dump(char_to_idx, f)
 
     vocab_size = len(char_to_idx)
 
@@ -70,23 +90,27 @@ def train(text, epochs=100, save_freq=10):
                 BATCH_SIZE, SEQ_LENGTH, vocab_size)
     model = build_model(BATCH_SIZE, SEQ_LENGTH, vocab_size)
     model.summary()
+    if last_epoch_checkpointed>0:
+        load_weights(last_epoch_checkpointed,model)
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
 
-    json.dump(json.loads(model.to_json()),
-              Path(MODEL_DIR).joinpath('model.json').open('w'),
-              sort_keys=True,
-              indent=1
-              )
 
-    plot_model(model, to_file=os.path.join(MODEL_DIR, 'model_plot.svg'), show_shapes=True, show_layer_names=True)
+    if not resume:
+        json.dump(json.loads(model.to_json()),
+                Path(MODEL_DIR).joinpath('model.json').open('w'),
+                sort_keys=True,
+                indent=1
+                )
+
+        plot_model(model, to_file=os.path.join(MODEL_DIR, 'model_plot.svg'), show_shapes=True, show_layer_names=True)
 
     T = np.asarray([char_to_idx[c] for c in text], dtype=np.int32)
     steps_per_epoch = (len(text) / BATCH_SIZE - 1) / SEQ_LENGTH
     logger.info("steps_per_epoch=%d", steps_per_epoch)
-    log = TrainLogger('training_log.csv')
+    log = TrainLogger('training_log.csv', last_epoch_checkpointed)
 
-    for epoch in range(epochs):
+    for epoch in range(last_epoch_checkpointed, epochs):
         print('\nEpoch {}/{}'.format(epoch + 1, epochs))
         losses, accs = [], []
 
@@ -113,12 +137,14 @@ if __name__ == '__main__':
                         help='number of epochs to train for')
     parser.add_argument('--freq', type=int, default=10,
                         help='checkpoint save frequency')
+    parser.add_argument('--resume', action='store_true',
+                        help='Try to resume from previously interrupted training and continue to train until epoch')
     args = parser.parse_args()
 
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
 
     logger.info("Beginning to train...")
-    train(open(os.path.join(DATA_DIR, args.input)).read(), args.epochs, args.freq)
+    train(open(os.path.join(DATA_DIR, args.input)).read(), args.epochs, args.freq, args.resume)
 
     logger.info("End of training.")
